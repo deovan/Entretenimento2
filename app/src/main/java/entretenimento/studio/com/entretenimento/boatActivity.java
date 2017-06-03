731b1dd7b100e74cc9702ae08089cb51677748c0
Deovan Zanol <dedezanol9@gmail.com>
2017-06-03 17:06:13 -0300

    Finalização dos menus... list view

--- app/src/main/java/entretenimento/studio/com/entretenimento/boatActivity.java
+++ app/src/main/java/entretenimento/studio/com/entretenimento/boatActivity.java
@@ -1,8 +1,15 @@
 package entretenimento.studio.com.entretenimento;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -10,7 +17,7 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class boatActivity extends AppCompatActivity {
+public class boatActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,21 @@ public class boatActivity extends AppCompatActivity {
         ArrayList<aEstabelecimento> est = adicionarEst();
         ArrayAdapter adapter = new aEstabelecimentoAdapter(this, est);
         lista.setAdapter(adapter);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+
 
         lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -60,5 +82,88 @@ public class boatActivity extends AppCompatActivity {
 
         return est;
     }
+
+
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_sem_settings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_inicio) {
+
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+
+        } else if (id == R.id.nav_bar) {
+
+            Intent intent = new Intent(this, barActivity.class);
+            startActivity(intent);
+
+        } else if (id == R.id.nav_boate) {
+
+            Intent intent = new Intent(this, boatActivity.class);
+            startActivity(intent);
+
+        } else if (id == R.id.nav_cinema) {
+
+            Intent intent = new Intent(this, cinemaActivity.class);
+            startActivity(intent);
+
+        } else if (id == R.id.nav_Pub) {
+
+            Intent intent = new Intent(this, pubActivity.class);
+            startActivity(intent);
+
+        } else if (id == R.id.nav_contato) {
+
+            Intent intent = new Intent(this, contatoActivity.class);
+            startActivity(intent);
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+
+
+
+
+}
+
+
 
