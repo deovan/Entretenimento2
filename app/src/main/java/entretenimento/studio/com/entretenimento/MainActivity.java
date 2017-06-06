bdcf95187f4e4255da6ccc93629d0d5e8383cda5
Deovan Zanol <dedezanol9@gmail.com>
2017-06-05 23:55:42 -0300

    removido inicio activty

--- app/src/main/java/entretenimento/studio/com/entretenimento/MainActivity.java
+++ app/src/main/java/entretenimento/studio/com/entretenimento/MainActivity.java
@@ -53,15 +53,6 @@ public class MainActivity extends AppCompatActivity
             super.onBackPressed();
         }
     }
-   /* public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-           drawer.closeDrawer(GravityCompat.START);
-        } else {
-         super.onPause();
-        }
-    }*/
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -92,7 +83,7 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_inicio) {
 
-            Intent intent  = new Intent(this, inicioActivity.class);
+            Intent intent  = new Intent(this, MainActivity.class);
             startActivity(intent);
 
 
